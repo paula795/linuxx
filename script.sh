0fa18dd177fa28f7061d6d8bc63bd09c1bc00d91
paula795 <pasimbanape@uide.edu.ec>
2025-12-03 15:47:21 +0000

    arquiscript1

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #Crear y listar archivos
-echo "Hola LInux">archivo.txt #El echo es un comando que imprime texto
+echo "Hola LInux">archivo.txt 
+#El echo es un comando que imprime texto
+#"Hola LInux" es la cadena de texto que se va a imprimir
+#> es la redirección de salida, toma lo que imprime echo y en vez de mostrarlo en pantalla, lo guarda en un archivo.
+#archivo.txt → nombre del archivo de texto donde se guarda el contenido.
 cat archivo.txt
 cp archivo.txt copia.txt
 ls -l
